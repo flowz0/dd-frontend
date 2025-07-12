7949cd0adbd788c8dcc7acd12c698f2201801e70
flowz0 <billylflowers@gmail.com>
2025-07-12 12:26:12 -0700

    add subtitle to Header on Blog page

--- app/(pages)/blog/page.tsx
+++ app/(pages)/blog/page.tsx
@@ -9,11 +9,11 @@ export default function Blog() {
     <>
       <article>
         <div>
-          <Header gradientBg>
-            Discover our latest blogs
+          <Header subtitle="Explore expert tips, seasonal advice, and must-know facts about air quality, duct maintenance, and home comfort straight from Kansas City's duct cleaning professionals." gradientBg>
+            Fresh Insights for a Cleaner Home
           </Header>
           <div className="bg-[#ffffff] px-6">
-            <div className="pt-12 pb-20 flex flex-col gap-y-16 max-w-7xl mx-auto">
+            <div className="pt-8 pb-20 flex flex-col gap-y-16 max-w-7xl mx-auto sm:pt-12">
               <div>
                 <h2 className="text-2xl font-semibold md:text-3xl">Featured blogs</h2>
                 <FeaturedBlogs />
