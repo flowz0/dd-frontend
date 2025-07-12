c938623059c78dd472e5df0890c5c61e294f510b
flowz0 <billylflowers@gmail.com>
2025-07-12 12:31:14 -0700

    improve Service component style customization

--- app/components/pages/landing/Service.tsx
+++ app/components/pages/landing/Service.tsx
@@ -8,14 +8,14 @@ interface ServiceProps {
 
 export default function Service({ img, title, desc }: ServiceProps) {
   return (
-    <div className="max-w-sm rounded-xl flex flex-col items-center">
+    <div className="bg-[hsl(0,0%,100%)] py-12 px-8 max-w-sm rounded-lg flex flex-col items-center shadow shadow-[hsl(0,0%,80%)]">
       <Image
         src={img}
         alt={title + "image"}
         className="rounded-lg h-20 w-auto"
         draggable={false}
       />
-      <h3 className="mt-6 text-xl font-semibold text-center sm:text-2xl">
+      <h3 className="mt-8 text-xl font-semibold text-center sm:text-2xl">
         {title}
       </h3>
       <p className="mt-2 text-center">
