e4c20272720e263f492c6014a0d2b757d8499e37
flowz0 <billylflowers@gmail.com>
2025-07-12 12:28:32 -0700

    improve Header component style customization

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,13 +6,13 @@ interface HeaderProps {
 
 export default function Header({ children, subtitle, gradientBg }: HeaderProps) {
   return (
-    <header className={`text-[#333333] ${gradientBg ? "bg-gradient-to-b from-[#E6E6E6] to-[#ffffff]" : ""} pt-20`}>
-      <div className="px-6 pt-12 pb-8 max-w-7xl mx-auto md:py-20">
-        <h1 className="text-4xl font-semibold text-center sm:text-5xl">
+    <header className={`text-[#333333] ${gradientBg ? "bg-gradient-to-b from-[#E6E6E6] to-[#ffffff]" : ""}`}>
+      <div className="pt-32 px-6 max-w-7xl mx-auto sm:pt-40">
+        <h1 className="text-4xl font-semibold text-center max-w-md mx-auto sm:text-5xl">
           {children}
         </h1>
         {subtitle ? (
-          <p className="mt-4 max-w-lg mx-auto text-center">{subtitle}</p>
+          <p className="mt-4 max-w-2xl mx-auto text-center">{subtitle}</p>
         ) : ""}
       </div>
     </header>
