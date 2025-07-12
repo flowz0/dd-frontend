7f60c6cec02e7a937ca71a01fa4b7bc24ab42ec5
flowz0 <billylflowers@gmail.com>
2025-07-12 12:32:30 -0700

    improve Stat component with lighter blue for better contrast

--- app/components/pages/landing/Stat.tsx
+++ app/components/pages/landing/Stat.tsx
@@ -8,7 +8,7 @@ export default function Stat({stat, title, desc}: StatProps) {
   return (
     <div>
       <h3 className="text-xl font-semibold max-w-sm text-center sm:text-2xl">
-        <span className="text-[#0080DB] font-bold">{stat}</span> {title}
+        <span className="text-[hsl(205,100%,68%)] font-bold">{stat}</span> {title}
       </h3>
       <p className="max-w-xs text-center mt-3">
         {desc}
