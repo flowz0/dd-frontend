ae5ede7706239f88c749da2f98871216bc48fafb
flowz0 <billylflowers@gmail.com>
2025-07-12 23:38:16 -0700

    import Reviews assets

--- data/reviews.ts
+++ data/reviews.ts
@@ -1,6 +1,15 @@
 import { StaticImageData } from "next/image";
 
-import PlaceholderImg from "@/public/1000x1000.svg";
+import LaurelHerz from "@/public/reviews/LaurelHerz.png"
+import PenelopeTilbury from "@/public/reviews/PenelopeTilbury.png"
+import PJB from "@/public/reviews/PJB.png"
+import RayRuark from "@/public/reviews/RayRuark.png"
+import RebeccaGreen from "@/public/reviews/RebeccaGreen.png"
+import MattScrogham from "@/public/reviews/MattScrogham.png"
+import FrankieMalena from "@/public/reviews/FrankieMalena.png"
+import TammyMyersHanson from "@/public/reviews/TammyMyersHanson.png"
+import ChrisBryant from "@/public/reviews/ChrisBryant.png"
+import JoeBlack from "@/public/reviews/JoeBlack.png"
 
 export interface Review {
   id: number;
@@ -16,35 +25,35 @@ export const topRowReviews: Review[] = [
     name: "Laurel Herz",
     // brand: "Blue Springs",
     message: "They were awesome!! Very quick and cleaned our returns and vents very well. Highly recommend them!!",
-    avatar: PlaceholderImg
+    avatar: LaurelHerz
   },
   {
     id: 2, 
     name: "Penelope Tilbury",
     // brand: "Blue Springs",
     message: "Duct Daddy was efficient and professional in cleaning my vents. I was impressed with his personality and skills.",
-    avatar: PlaceholderImg
+    avatar: PenelopeTilbury
   },
   {
     id: 3, 
     name: "PJ B",
     // brand: "Blue Springs",
     message: "The young men did a very nice job of cleaning and sanitizing my air ducts. I highly recommend!",
-    avatar: PlaceholderImg
+    avatar: PJB
   },
   {
     id: 4, 
     name: "Ray Ruark",
     // brand: "Blue Springs",
     message: "They showed up , were very friendly and really helped us out. Had birds nest in a vent. Would highly recommend them.",
-    avatar: PlaceholderImg
+    avatar: RayRuark
   },
   {
     id: 5, 
     name: "Rebecca Green",
     // brand: "Blue Springs",
     message: "Not only did duct daddy do a great job with our vents…. The owner Was super knowledgeable and easy to work with!",
-    avatar: PlaceholderImg
+    avatar: RebeccaGreen
   },
 ];
 
@@ -54,34 +63,34 @@ export const bottomRowReviews: Review[] = [
     name: "Matt Scrogham",
     // brand: "Lee's Summit",
     message: "The Team at Duct Daddy was awesome! They went through each vent of the house and cleaned everything.",
-    avatar: PlaceholderImg
+    avatar: MattScrogham
   },
   {
     id: 2, 
     name: "Frankie Malena",
     // brand: "Lee's Summit",
     message: "Great experience, friendly customer service and amazing results! Recommend if your in need of duct cleaning!!",
-    avatar: PlaceholderImg
+    avatar: FrankieMalena
   },
   {
     id: 3, 
     name: "Tammy Myers Hanson",
     // brand: "Lee's Summit",
     message: "Highly recommend!! Thorough, reliable, friendly, and responsive. Breathing easier because of him!!",
-    avatar: PlaceholderImg
+    avatar: TammyMyersHanson
   },
   {
     id: 4, 
     name: "Chris Bryant",
     // brand: "Lee's Summit",
     message: "Great communication! Professional and efficient from start to finish. Definitely recommend",
-    avatar: PlaceholderImg
+    avatar: ChrisBryant
   },
   {
     id: 5, 
     name: "Joe Black",
     // brand: "Lee's Summit",
     message: "Finding good honest reliable contractors of any trade is a chore. I don't think you'll go wrong here!!!",
-    avatar: PlaceholderImg
+    avatar: JoeBlack
   },
 ];
