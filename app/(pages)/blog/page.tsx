02776964036a4826de52f4dc4474dfe58fe8e5b3
flowz0 <billylflowers@gmail.com>
2025-07-12 22:30:38 -0700

    remove FeaturedBlogs component and implementation

--- app/(pages)/blog/page.tsx
+++ app/(pages)/blog/page.tsx
@@ -1,6 +1,5 @@
 import CTA from "@/app/components/CTA";
 import AllBlogs from "@/app/components/pages/blog/AllBlogs";
-// import FeaturedBlogs from "@/app/components/pages/blog/FeaturedBlogs";
 import Header from "@/app/components/Header";
 import FAQs from "@/app/components/pages/landing/FAQs";
 
@@ -14,10 +13,6 @@ export default function Blog() {
           </Header>
           <div className="bg-[#ffffff] px-6">
             <div className="pt-8 pb-20 flex flex-col gap-y-16 max-w-7xl mx-auto sm:pt-12">
-              {/* <div> */}
-                {/* <h2 className="text-2xl font-semibold md:text-3xl">Featured blogs</h2> */}
-                {/* <FeaturedBlogs /> */}
-              {/* </div> */}
               <div>
                 <h2 className="text-2xl font-semibold md:text-3xl">All blogs</h2>
                 <AllBlogs />
