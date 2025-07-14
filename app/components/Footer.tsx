f73cc649956e470378ae9442a7cafc923db6f344
flowz0 <billylflowers@gmail.com>
2025-07-14 16:21:40 -0700

    fix: update copyright in Footer component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -51,7 +51,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="flex flex-col gap-2 mt-20 sm:mt-32 md:justify-between md:flex-row">
-          <p>© 2025 Duct Daddy</p>
+          <p>© 2025 Duct Daddy Duct Cleaning</p>
           <div className="flex flex-col gap-2 md:gap-8 md:flex-row">
             <Link href="/#faqs">Terms of Service</Link>
             <Link href="/#faqs">Privacy Policy</Link>
