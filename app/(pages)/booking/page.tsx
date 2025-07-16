b15eabfee39f2bd0d2051e4fb30b71d70e861051
flowz0 <billylflowers@gmail.com>
2025-07-15 21:48:21 -0700

    fix: improve UX for BookingPage

--- app/(pages)/booking/page.tsx
+++ app/(pages)/booking/page.tsx
@@ -44,27 +44,27 @@ export default function BookingPage() {
 
   return (
     <>
-    <main className="bg-[#16385B] text-[#ffffff] pt-36 pb-28 px-6 sm:pt-44 sm:pb-32">
-      <header className="max-w-7xl mx-auto flex flex-col items-center">
-        <h2 className="text-4xl font-semibold text-center sm:text-5xl">Online Booking</h2>
-        <p className="mt-4 text-center max-w-xl mx-auto">
-          Schedule an appointment with Duct Daddy Duct Cleaning.
-        </p>
-        <button
-          data-token="127343b956e24e13a52da46f4500936b"
-          data-orgname="Duct-Daddy"
-          onClick={handleClick}
-          className="flex items-center gap-x-3 mt-12 bg-[#0080DB] text-[#E6E6E6] py-3 px-6 font-semibold rounded-md hover:bg-[hsl(205,100%,33%)] active:bg-[hsl(205,100%,23%)]"
-        >
-          <FaCalendarDays className="w-4 h-4" />
-          Book appointment
-        </button>
-      </header>
-    </main>
-    <ReviewSection />
-    <ServiceArea />
-    <FAQs />
-    <CTA />
+      <main className="bg-[#ffffff] text-[#333333] pt-28 pb-32 sm:pt-32">
+        <header className="max-w-7xl px-6 mx-auto flex flex-col items-center">
+          <h2 className="text-4xl font-semibold text-center sm:text-5xl">Online Booking</h2>
+          <p className="mt-4 text-center max-w-xl mx-auto">
+            Schedule an appointment with Duct Daddy Duct Cleaning.
+          </p>
+          <button
+            data-token="127343b956e24e13a52da46f4500936b"
+            data-orgname="Duct-Daddy"
+            onClick={handleClick}
+            className="flex items-center gap-x-3 mt-8 bg-[#0080DB] text-[#E6E6E6] py-3 px-6 font-semibold rounded-md hover:bg-[hsl(205,100%,33%)] active:bg-[hsl(205,100%,23%)]"
+          >
+            <FaCalendarDays className="w-4 h-4" />
+            Book appointment
+          </button>
+        </header>
+      </main>
+      <ReviewSection />
+      <ServiceArea />
+      <FAQs />
+      <CTA />
     </>
   )
 }
