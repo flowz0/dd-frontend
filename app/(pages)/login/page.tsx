784ed854665dc5ee030882f42ff2f6fd92cad371
flowz0 <billylflowers@gmail.com>
2025-07-15 21:48:33 -0700

    fix: consistent responsive padding in LoginPage

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/app/components/pages/dashboard/LoginForm";
 
 export default function LoginPage() {
   return (
-    <main className="pt-32 pb-16 px-6 max-w-7xl mx-auto lg:pt-40">
+    <main className="px-6 pt-28 pb-32 max-w-7xl mx-auto sm:pt-32">
       <div className="flex flex-col gap-8 md:flex-row">
         <header className="w-full md:w-1/2">
           <h1 className="text-4xl font-semibold text-center md:text-5xl md:text-start">
