0568da316290e123d4447aa0780a5ba7a6a8cbbb
flowz0 <billylflowers@gmail.com>
2025-07-18 17:21:25 -0700

    fix: optimize gradient on mobile and desktop devices in ReviewSection component

--- app/components/pages/landing/ReviewSection.tsx
+++ app/components/pages/landing/ReviewSection.tsx
@@ -7,7 +7,7 @@ export default function ReviewSection() {
       <h2 className="text-3xl font-semibold text-center sm:text-4xl">
         What Our <span className="text-[#0080DB] font-bold">Customers</span> Are Saying
       </h2>
-      <div className="[mask-image:_linear-gradient(to_right,_transparent_0,_#e6e6e6_256px,#e6e6e6_calc(100%-256px),_transparent_100%)] mt-8 flex flex-col gap-2 relative sm:mt-12">
+      <div className="[mask-image:_linear-gradient(to_right,_transparent_0,_#e6e6e6_96px,#e6e6e6_calc(100%-96px),_transparent_100%)] mt-8 flex flex-col gap-2 relative sm:mt-12 sm:[mask-image:_linear-gradient(to_right,_transparent_0,_#e6e6e6_256px,#e6e6e6_calc(100%-256px),_transparent_100%)]">
         <ReviewRow reviews={topRowReviews} direction="right" />
         <ReviewRow reviews={bottomRowReviews} direction="left" />
       </div>
