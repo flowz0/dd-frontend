fc95dccd1eb635bed1beeb18310c1d42682e7416
flowz0 <billylflowers@gmail.com>
2025-07-19 17:38:17 -0700

    fix: decrease desktop padding in Stats component

--- app/components/pages/landing/Stats.tsx
+++ app/components/pages/landing/Stats.tsx
@@ -2,7 +2,7 @@ import Stat from "./Stat";
 
 export default function Stats() {
   return (
-    <section className="bg-[#16385B] text-[#ffffff] py-20 px-6 flex flex-col items-center justify-center sm:py-32">
+    <section className="bg-[#16385B] text-[#ffffff] py-20 px-6 flex flex-col items-center justify-center sm:py-24">
       <div className="max-w-7xl mx-auto">
         <div className="flex flex-wrap justify-center flex-col gap-24 md:flex-row">
           <Stat
